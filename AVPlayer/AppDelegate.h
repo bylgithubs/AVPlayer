75dea4916f265f30740c1075ee4abc6c593c9610
liaobaoyu <1576660896@qq.com>
2021-05-10 17:10:14 +0800

    Initial Commit

---
+++ AVPlayer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AVPlayer
+//
+//  Created by BaoYu Liao on 2021/5/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AVPlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by BaoYu Liao on 2021/5/10.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
